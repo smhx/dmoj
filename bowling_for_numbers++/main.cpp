d04898114a96a53adc2e8f70113bb5567d21a8b1
Steven Mai <stevenmai132@yahoo.ca>
2017-10-22 11:04:45 -0400

    removed old code

--- bowling_for_numbers++/main.cpp
+++ bowling_for_numbers++/main.cpp
@@ -1,8 +1,9 @@
 #include <cstdio>
 #include <algorithm>
 #define MAXN 10600
-using namespace std;
-int t, n, k, w, A[MAXN], dp[MAXN][2][2], best[MAXN], sum[MAXN];
+#define MAXK 505
+using std::max;
+int t, n, k, w, A[MAXN], dp[MAXN][MAXK][2], best[MAXN], sum[MAXN];
 int main() {
     scanf("%d", &t);
     while (t--) {
@@ -10,38 +11,31 @@ int main() {
         for (int i = 1; i <= n; i++) scanf("%d", A+i);
         for (int i = 1; i <= n; ++i) sum[i] = sum[i-1]+A[i];
         for (int h = 1; h <= n; ++h) {
-            int b=0; best[h]=0;
-            for (int j = 1; j <= w && j <= h; ++j) {
-                int cursum=sum[h]-sum[h-j];
-                if (cursum > b) b = cursum, best[h] = j;
-            }
-        }
-        for (int s = 1; s <= k; ++s) {
-            for (int h = 1; h <= n; ++h) {
-                for (int l = 0; l < 2; ++l) {
-                    // what if best[h]=0?
-                    
-                    int next = (l) ? h-best[h] : max(0, h-w);
-                    int leave = dp[h-1][s%2][0];
-                    int take = sum[h]-sum[next]+dp[next][(s-1)%2][1];
-                    dp[h][s%2][l] = max(take, leave);
+            if (best[h-1]<w) {
+                best[h] = (sum[h]-sum[h-best[h-1]]>0) ? best[h-1]+1 : 0;
+            } else {
+                int b=0; best[h]=0;
+                for (int j = 1; j <= w && j <= h; ++j) {
+                    int cursum=sum[h]-sum[h-j];
+                    if (cursum > b) b = cursum, best[h] = j;
                 }
             }
         }
-        printf("%d\n", dp[n][k%2][1] );
+        for (int h = 1; h <= n; ++h) {
+            for (int s = 1; s <= k; ++s) {
+                for (int l = 0; l < 2; ++l) {
+                    int &c = dp[h][s][l];
+                    c = dp[h-1][s][0];
+                    if (l) {
+                        for (int next = h-1; next >= h-w && next > 0; --next) c = max(c, sum[h]-sum[next]+dp[next][s-1][1]);
+                    } else {
+                        int next = max(0, h-w);
+                        c = max(c, sum[h]-sum[next]+dp[next][s-1][1]);
+                    }
+                }
+            }
+        }
+        // you only ever need s and s-1.
+        printf("%d\n", dp[n][k][1]);
     }
 }
-
-/*
- TC 1:
-    41059
-    113735
-    139975
-    153154
- TC 2:
-    226173
-    224664
-    229045
-    271413
-
-*/

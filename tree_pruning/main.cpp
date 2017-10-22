9d0482f3f12d6ab0457f36b04c94afc5db452c12
Steven Mai <stevenmai132@yahoo.ca>
2017-10-22 14:06:45 -0400

    tree pruning

--- tree_pruning/main.cpp
+++ tree_pruning/main.cpp
@@ -1,12 +1,15 @@
 #include <cstdio>
 #include <vector>
 #define INF 1000000009
-#define MAXN 205
+#define MAXN 305
 using namespace std;
 
 
 int N, D, color[MAXN], w[MAXN], b[MAXN], dp[MAXN][2*MAXN];
 vector<int> ch[MAXN];
+
+// color is 1 for white, 0 for black
+// difference is white - black
 
 void dfs(int u) {
 
@@ -19,33 +22,62 @@ void dfs(int u) {
         b[u] += b[v];
     }
 
-    dp[u][w[u]-b[u]+N]=0;
+    dp[u][w[u]-b[u]+N] = 0;
 
-    int delta = color[u]==1 ? -1 : 1;
-    for (int v : ch[u]) {
-        for (int s = 0; s <= N+D; ++s)
-            dp[u][s+delta] = min(dp[u][s+delta], dp[v][s]);
+    for (int v : ch[u]){
+        int diff = (w[u]-w[v])-(b[u]-b[v]) + N;
+        dp[u][diff]=min(dp[u][diff], 1);
+    }
+
+    int delta = color[u]==0 ? -1 : 1;
+
+    if (ch[u].size()==2) {
+        dp[u][delta+N] = min(dp[u][delta+N], 2);
     }
 
 
-    if (ch[u].size()==2) {
-        int v = ch[u][0], w = ch[u][1];
-        // you can make a sum lft on left and rt on right
-        for (int lft = 1; lft <= N; ++lft) {
-            for (int rt = 1; rt <= N; ++rt) {
-                dp[u][lft+rt+delta] = min(dp[u][lft+rt+delta], );
+    for (int v : ch[u]) {
+        for (int s = 0; s <= 2*N; ++s) {
+            int diff = (w[u]-w[v])-(b[u]-b[v])+s;
+            if (diff>=0 && diff <= 2*N) {
+                dp[u][diff] = min(dp[u][diff], dp[v][s]);
+            }
+        }
+
+    }   
+
+    // Case 4: You cut one edge, and cut inside the other
+    for (int v : ch[u]) {
+        for (int w : ch[u]) {
+            if (v==w) continue;
+            for (int s = 0; s <= 2*N; ++s) {
+                int diff = delta + s;
+                if (diff>=0 && diff <= 2*N) 
+                    dp[u][diff] = min(dp[u][diff], dp[w][s]+1);
             }
         }
     }
 
+    // Case 4: You cut both inside
+    if (ch[u].size()==2) {
+        int v = ch[u][0], w = ch[u][1];
+        for (int sv = 0; sv <= 2*N; ++sv) {
+            for (int sw = 0; sw <= 2*N; ++sw) {
+                int diff = sv+sw+delta-N;
+                if (diff>=0 && diff <= 2*N) 
+                    dp[u][diff] = min(dp[u][diff], dp[w][sw]+dp[v][sv]);
+            }
+        }
+    }
 }
 
 
 int main() {
+    freopen("data.txt", "r", stdin);
     scanf("%d%d", &N, &D);
 
     for (int u = 0; u < N; ++u)
-        for (int s = 0; s <= N+D; ++s)
+        for (int s = 0; s <= 2*N; ++s)
             dp[u][s] = INF;
 
     for (int i = 0; i < N; ++i) {
@@ -56,8 +88,6 @@ int main() {
             ch[u].push_back(v);
         }
     }
-
-    // dp[u][s] to make s-N in T[u]
     dfs(0);
-
+    printf("%d\n", dp[0][D+N] == INF ? -1 : dp[0][D+N]);
 }

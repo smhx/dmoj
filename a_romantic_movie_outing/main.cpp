70c9abba3fbbece7dedd00f2129081fe180c4d8e
Steven Mai <stevenmai132@yahoo.ca>
2017-11-22 22:45:35 -0500

    a romantic movie outing

--- a_romantic_movie_outing/main.cpp
+++ a_romantic_movie_outing/main.cpp
@@ -1,43 +1,57 @@
 #include <cstdio>
-const int MAXL = 1003, BIT_SIZE = 2*MAXL;
-int L, T;
+const int MAXL = 1003, BIT_SIZE = 4*MAXL, INF = 1000000000;
+int L, T, bit1[BIT_SIZE][BIT_SIZE], bit2[BIT_SIZE][BIT_SIZE], grid[BIT_SIZE][BIT_SIZE];
 
-int bit1[BIT_SIZE][BIT_SIZE], bit2[BIT_SIZE][BIT_SIZE];
-void update(int (&bit)[BIT_SIZE][BIT_SIZE], int x, int y, int v) {
-	for (; x < BIT_SIZE; x += x&-x) {
-		for (; y < BIT_SIZE; y += y&-y) {
-			bit[x][y] += v;
-		}
-	}
+void update(int (&bit)[BIT_SIZE][BIT_SIZE], int x, int z, int v) {
+	for (; x < BIT_SIZE; x += x&-x) 
+		for (int y = z; y < BIT_SIZE; y += y&-y) bit[x][y] += v;
 }
 
-int sum(int (&bit)[BIT_SIZE][BIT_SIZE], int x, int y) {
+int sum(int (&bit)[BIT_SIZE][BIT_SIZE], int x, int z) {
 	int res = 0;
-	for (; x; x -= x&-x) {
-		for (; y; y -= y&-y) {
-			res += bit[x][y];
-		}
-	}
+	for (; x; x -= x&-x) 
+		for (int y = z; y; y -= y&-y) res += bit[x][y];
 	return res;
 }
 
-int val(int r, int c) {
-
+int pyramid(int r, int c) {
+	int x = sum(bit1, r+c-1, r);
+	int y = sum(bit2, BIT_SIZE-1, r);
+	int z = sum(bit2, r-c+1000, r);
+	return x-y+z;
 }
+
 int main() {
+	freopen("data.txt", "r", stdin);
 	scanf("%d%d", &L, &T);
 	while (T--) {
 		char cmd; int r,c; scanf(" %c %d %d", &cmd, &r, &c);
 		if (cmd=='E') {
-			update(bit1, r+c-1, c, 1);
-			update(bit2, r-c+L, c, 1);
+			update(bit1, r+c-1, r, 1);
+			update(bit2, r-c+1000, r, 1);
+			if (r < BIT_SIZE) grid[r][c] = 1;
 		} else if (cmd=='L') {
-			update(bit1, r+c-1, c, -1);
-			update(bit2, r-c+L, c, -1);
+			update(bit1, r+c-1, r, -1);
+			update(bit2, r-c+1000, r, -1);
+			if (r < BIT_SIZE) grid[r][c] = 0;
 		} else {
-			if (val(r,c)) {
-				printf("No\n");
+			if (grid[r][c] || grid[r][c+1]) printf("No\n");
+			else printf("%d\n", pyramid(r,c)+pyramid(r, c+1));
+		}
+	}
+
+	int b1 = INF, b2 = INF;
+
+	for (int r = L+1; r <= L+1001; ++r) {
+		for (int c = 1; c <= 1000; ++c) {
+			if (!grid[r][c]) {
+				int x = pyramid(r,c);
+				if (x<=b1) {
+					b2 = b1;
+					b1 = x;
+				} else if (x < b2) b2 = x;
 			}
 		}
 	}
+	printf("%d\n", b1+b2);
 }

ddcb13b57adeaae525c28a0dfd391fdde353120b
Steven Mai <stevenmai132@yahoo.ca>
2017-11-25 15:39:00 -0500

    splitting hares

--- splitting_hares/main.cpp
+++ splitting_hares/main.cpp
@@ -1,99 +1,104 @@
 #include <cstdio>
+#include <cmath>
 #include <algorithm>
 #include <vector>
-#include <deque>
-#include <cmath>
-
 using namespace std;
 const int MAXN = 4003;
-const double PI = 3.141592653589793, EPS = 1e-6;
+const double PI = atan(1) * 4.0, EPS = 1e-6;
+struct point {double x,y;} p[MAXN];
+int N, best = -1000000000, w[MAXN];
+double angle(point p1, point p2) {
+	if (fabs(p1.x-p2.x) <= EPS) {
+		if (p1.y < p2.y) return PI/2.0;
+		else return 3.0*PI/2.0;
+	}
+	if (p1.x < p2.x) {
+		double theta = atan((p2.y-p1.y)/(p2.x-p1.x));
+		if (theta >= 0) return theta;
+		else return theta+2*PI;
+	}
 
-
-struct bunny {
-	double x, y;
-	int g;
-} p[MAXN];
-
-typedef pair<double, int> pdi;
-
-int N, M =0, T=0, sum[MAXN];
-
-double ang(bunny to, bunny from) {
-	// angle with x axis
-	if (abs(to.x-from.x) <= EPS) {
-		return to.y > from.y ? PI/2.0 : 1.5*PI;
-	} 
-	if (to.x > from.x) {
-		double tangent = (to.y-from.y) / (to.x-from.x);
-		double a = atan(tangent);
-		if (a<0) a+=2*PI;
-		return a;
-	} 
-	double tangent = (to.y-from.y) / (from.x-to.x);
-	double a = atan(tangent);
-	a = PI-a;
-	return a;
+	double theta = atan((p2.y-p1.y)/(p1.x-p2.x));
+	return PI-theta;
 }
 
-inline double cwrot(double a1, double a2) {
-	return a2-a1 >= 0 ? a2-a1 : a2-a1+2*PI;
+inline int sumFromTo(int a, int b,const vector<int>& presum) {
+	if(a<=b) {
+		if (a==0) return presum[b];
+		else return presum[b]-presum[a-1];
+	}
+
+	return presum[b] + presum.back() - presum[a-1];
 }
+
+void solve(const vector<pair<double,int> >& q, int i) {
+
+	if (q[0].first >= PI) return;
+
+	vector<pair<double, int> > l;
+
+	l.push_back({q[0].first, q[0].second});
+	for (int i = 1; i < q.size(); ++i) {
+		if (fabs(q[i].first-l.back().first) <= EPS) l.back().second+=q[i].second;
+		else l.push_back({q[i].first, q[i].second});
+	}
+
+	vector<int> presum(l.size());
+
+	presum[0] = l[0].second;
+	for (int i = 1; i < l.size(); ++i) presum[i] = presum[i-1] + l[i].second;
+	
+	int a = 0, b = 0;
+	while (1) {
+		int nxt = (b+1)%l.size();
+		if (nxt==a) break;
+		if (l[nxt].first > l[a].first + PI) break;
+		b = nxt;
+	}
+	int suma=0, sumb=0;
+	suma = sumFromTo(a,b,presum);
+	if (fabs(l[b].first - l[a].first - PI) <= EPS) sumb = sumFromTo(b, a, presum);
+	else sumb = sumFromTo(b+1, a, presum);
+
+	best = max(best, max(w[i],0) + max(suma, sumb));
+	for (++a; l[a].first < PI && a < l.size(); ++a) {
+		while (1) {
+			int nxt = (b+1)%l.size();
+			if (nxt==a) break;
+			if (!( (l[nxt].first > l[a-1].first + PI) && l[nxt].first <= l[a].first+PI) ) break;
+			b = nxt;
+		}
+		suma = sumFromTo(a,b,presum);
+		if (fabs(l[b].first - l[a].first - PI) <= EPS) sumb = sumFromTo(b, a, presum);
+		else sumb = sumFromTo(b+1, a, presum);
+
+		best = max(best, max(w[i],0) + max(suma, sumb));
+	}
+}
+
 
 int main() {
-	freopen("data.txt", "r", stdin);
+// 	freopen("data.txt", "r", stdin);
 	scanf("%d", &N);
+	for (int i = 0; i < N; ++i) scanf("%lf%lf%d", &p[i].x, &p[i].y, w+i);
+
 	for (int i = 0; i < N; ++i) {
-		scanf("%lf%lf%d", &p[i].x, &p[i].y, &p[i].g);
-		T+=p[i].g;
-	}
-	printf("T = %d\n", T);
-	for (int i = 0; i < N; ++i) {
-		printf("i = %d, point %d (%lf, %lf)\n",i, p[i].g, p[i].x, p[i].y);
-		vector<pdi> v;
+		vector<pair<double, int> > q;
 		for (int j = 0; j < N; ++j) {
-			if (j!=i) {
-				double angle = ang(p[j], p[i]);
-				v.push_back({angle, j});
-			}	
-		}
-		sort(v.begin(), v.end());
-
-		for (int j = 0; j < v.size(); ++j) {
-			printf("	j=%d, point %d, angle %lf\n", j, p[v[j].second].g, v[j].first * 180.0 / PI);
-		}
-	
-
-		deque<int> dq;
-		dq.push_back(0);
-
-		int s = p[i].g + p[v[0].second].g;
-		for (int j = 1; j < v.size(); ++j) {
-			if (v[j].first - v[0].first > PI) break;
-			dq.push_front(j);
-			s += p[v[j].second].g;
-		}
-		printf("	s = %d, dq.size()=%lu\n", s, dq.size());
-		M = max(M, s);
-
-		for (int j = 1; j < v.size(); ++j) {
-			printf("  j = %d\n", j);
-			for (int k = dq.front()+1; cwrot(v[(dq.back()+1)%v.size()].first,v[k].first) <= PI+EPS; k = (k+1) % v.size()) {
-				s += p[v[k].second].g;
-				printf("	adding k=%d\n", k);
-				dq.push_front(k);
-				if (k==dq.back()) break;
+			if (i!=j) {
+				double theta = angle(p[i], p[j]);
+				q.push_back({theta, w[j]});
 			}
-			s -= p[v[j-1].second].g;
-			dq.pop_back();
-
-			printf("	s = %d, front=%d, back=%d, OTHER = %d\n", s, p[v[dq.front()].second].g, p[v[dq.back()].second].g,T-s + p[i].g + p[v[dq.back()].second].g );
-
-			M = max(M, s);
-			M = max(M, T-s + p[i].g + p[v[dq.back()].second].g);
 		}
+		sort(begin(q), end(q));
+		solve(q, i);
 
-		printf("M is now %d\n", M);
-
+		for (pair<double,int>& p : q) {
+			if (p.first >= PI) p.first -= PI;
+			else p.first += PI;
+		}
+		sort(begin(q), end(q));
+		solve(q, i);
 	}
-	printf("%d\n", M);
+	printf("%d\n", best);
 }

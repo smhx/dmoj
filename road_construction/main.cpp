d58df55817b2504143aa6c332589fa04c6e20510
Steven Mai <stevenmai132@yahoo.ca>
2017-12-22 12:33:50 -0500

    road construction

---
+++ road_construction/main.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+const int maxn = 100005;
+
+vector<int> adj[maxn], tree[maxn];
+int n, r, deg[maxn];
+
+int dfst=0, disc[maxn], low[maxn], p[maxn];	
+void tarjan(int u){
+	disc[u]=low[u]=++dfst;
+	for (int v : adj[u]) {
+		if (!disc[v]) {
+			p[v] = u;
+			tarjan(v);
+			low[u] = min(low[u], low[v]);
+		} else if (v!=p[u]){
+			low[u] = min(low[u], disc[v]);
+		}
+	}
+}
+
+int main() {
+	freopen("data.txt", "r", stdin);
+	int n,r;
+	scanf("%d%d", &n, &r);
+	for (int i = 0, u, v; i < r; ++i) {
+		scanf("%d%d", &u, &v);
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	tarjan(1);
+	for (int u = 1; u <= n; ++u) 
+		for (int v : adj[u]) 
+			if (low[u]!=low[v]) ++deg[low[u]];
+	int cnt = 0;
+	for (int u = 1; u <= n; ++u) if (deg[u]==1) ++cnt;
+	if (cnt%2==0) printf("%d\n", cnt/2);
+	else printf("%d\n", cnt/2+1);
+}

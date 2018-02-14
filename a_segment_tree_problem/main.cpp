5a9605c2d60fd94c174c999ce566629e7dec7ddc
Steven Mai <stevenmai132@yahoo.ca>
2018-02-13 21:50:54 -0500

    a segment tree problem

--- a_segment_tree_problem/main.cpp
+++ a_segment_tree_problem/main.cpp
@@ -3,159 +3,203 @@ using namespace std;
 
 typedef long long ll;
 
-const int MAX = 2e6+6, LOG = 20;
-
+const int MAX = 2e6+6;
+struct node {ll l, r, cnt, cost, lazy;} segt[8*MAX];
+const node NIL = node();
 int N;
 
-ll numcones=0, totcost=0;	
-char op[15];
+inline void combine(node a, node b, node& c) {
+	c.cnt = a.cnt + b.cnt;
+	c.cost = a.cost + b.cost;
+}
 
-struct bit {
-	ll b[MAX];
+void apply(int u, ll val) {
+	if (val == -1) {
+		segt[u].cnt = segt[u].cost = 0;
+		if (segt[u].l != segt[u].r) segt[u].lazy = -1;
+	} else {
+		segt[u].cnt += val*(segt[u].r-segt[u].l+1);
+		segt[u].cost += val*(segt[u].r*(segt[u].r+1)/2 - segt[u].l*(segt[u].l-1)/2);
+		if (segt[u].l != segt[u].r) {
+			if (segt[u].lazy == -1) {
+				segt[u].lazy = val;
+				segt[u<<1].lazy = -1;
 
-	// [1, MAX-1]
-
-	void update(int x, ll v) {
-		for (; x < MAX; x += x&-x) b[x]+=v;
-	}
-
-	ll sum(int x) {
-		ll res=0;
-		for (; x; x -= x&-x) res += b[x];
-		return res;
-	}
-
-	// returns the minimum index i such that sum(i) >= s
-	int lowerbound(ll s) {
-		ll cursum = 0; int x = 0;
-		for (int k = LOG-1; k >= 0; --k) {
-			if (x + (1<<k) < MAX) {
-				if (cursum + b[x + (1<<k) ] < s) {
-					x += 1<<k;
-				}
+				segt[u<<1|1].lazy = -1;
+				segt[u<<1].cnt = segt[u<<1].cost = segt[u<<1|1].cnt = segt[u<<1|1].cost = 0;
 			}
+			else segt[u].lazy += val;
 		}
-		return x+1;
 	}
-};
+}
 
-// coster computes ax+b.
+void pushdown(int u) {
+	apply(u<<1, segt[u].lazy);
+	apply(u<<1|1, segt[u].lazy);
+	segt[u].lazy=0;
+}
 
-bit counter, costm, costb;
+void build(int u, int l, int r) {
+	segt[u].l = l, segt[u].r = r;
+	if (l==r) {
+		segt[u].cost = segt[u].cnt = segt[u].lazy = 0;
+	} else {
+		int m = (l+r)/2;
+		build(u<<1, l, m); build(u<<1|1, m+1, r);
+		combine(segt[u<<1], segt[u<<1|1], segt[u]);
+	}
+}
 
+void update(int u, int l, int r, int val) {
+	if (segt[u].lazy!=0) pushdown(u);
 
-ll buylow(ll L) {
-	ll end = counter.lowerbound(L);
-	ll presum = counter.sum(end);
-	ll rem = L-presum;
+	if (l > segt[u].r || r < segt[u].l) return;
+	if (l <= segt[u].l && segt[u].r <= r) {
+		apply(u, val);
+	} else {
+		update(u<<1, l, r, val);
+		update(u<<1|1, l, r, val);
+		combine(segt[u<<1], segt[u<<1|1], segt[u]);
+	}
+}
 
-	ll cost = costm.sum(end-1)*(end-1) + costb.sum(end-1) + rem*end;
-	return cost;
+// not really necessary
+node query(int u, int l, int r) { 
+	if (r < segt[u].l || l > segt[u].r) return NIL;
+	if (segt[u].lazy!=0) pushdown(u);
+	if (l <= segt[u].l && segt[u].r <= r) return segt[u];
+	node left = query(u<<1, l, r), right = query(u<<1|1, l, r);
+	node x;
+	combine(left, right, x);
+	return x;
+}
+
+ll buyamt(int u, ll Q) {
+	if (segt[u].lazy!=0) pushdown(u);
+	ll numbought = 0;
+	if (segt[u].cost <= Q) {
+		numbought = segt[u].cnt;
+		segt[u].cnt = segt[u].cost = 0;
+		if (segt[u].l != segt[u].r) segt[u].lazy = -1;
+	} else {
+		if (segt[u].l == segt[u].r) {
+			numbought = Q/segt[u].l; // or r
+			segt[u].cnt -= numbought;
+			segt[u].cost -= numbought*segt[u].l;
+		} else {
+			if (segt[u<<1].cost >= Q) {
+				numbought = buyamt(u<<1, Q);
+			} else {
+				Q -= segt[u<<1].cost;
+				numbought = buyamt(u<<1, segt[u<<1].cost) + buyamt(u<<1|1, Q);
+			}
+			combine(segt[u<<1], segt[u<<1|1], segt[u]);
+		}
+	}
+	return numbought;
+}
+
+ll buylow(int u, ll L) {
+	if (segt[u].lazy!=0) pushdown(u);
+	ll paid = 0;
+	if (segt[u].cnt <= L) {
+		paid = segt[u].cost;
+		segt[u].cnt = segt[u].cost = 0;
+		if (segt[u].l != segt[u].r) segt[u].lazy = -1;
+	} else {
+		if (segt[u].l == segt[u].r) {
+			paid = segt[u].l * L;
+			segt[u].cnt -= L;
+			segt[u].cost -= L*segt[u].l;
+		} else {
+			if (segt[u<<1].cnt >= L) {
+				paid = buylow(u<<1, L);
+			} else {
+				L -= segt[u<<1].cnt;
+				paid = buylow(u<<1, segt[u<<1].cnt) + buylow(u<<1|1, L);
+			}
+			combine(segt[u<<1], segt[u<<1|1], segt[u]);
+		}
+	}
+	return paid;
+}
+
+ll buyhigh(int u, ll L) {
+	if (segt[u].lazy!=0) pushdown(u);
+	ll paid = 0;
+	if (segt[u].cnt <= L) {
+		paid = segt[u].cost;
+		segt[u].cnt = segt[u].cost = 0;
+		if (segt[u].l != segt[u].r) segt[u].lazy = -1;
+	} else {
+		if (segt[u].l == segt[u].r) {
+			paid = segt[u].l * L;
+			segt[u].cnt -= L;
+			segt[u].cost -= L*segt[u].l;
+		} else {
+			if (segt[u<<1|1].cnt >= L) {
+				paid = buyhigh(u<<1|1, L);
+			} else {
+				L -= segt[u<<1|1].cnt;
+				paid = buyhigh(u<<1|1, segt[u<<1|1].cnt) + buyhigh(u<<1, L);
+			}
+			combine(segt[u<<1], segt[u<<1|1], segt[u]);
+		}
+	}
+	return paid;
+}
+
+ll cost(int u, ll L) {
+	if (segt[u].lazy!=0) pushdown(u);
+	ll c;
+	if (segt[u].l == segt[u].r) c = segt[u].l;
+	else {
+		// check if it's on the left or right
+		if (segt[u<<1].cnt >= L) c = cost(u<<1, L);
+		else {
+			L -= segt[u<<1].cnt;
+			c = cost(u<<1|1, L);
+		}
+	}
+	return c;
 }
 
 int main() {
-	freopen("data.txt", "r", stdin);
-	scanf("%d", &N);
-	for (int q = 0; q < N; ++q) {
-		scanf(" %s", op);
-		if (strcmp(op, "ADD")==0) {
+	int Q; scanf("%d", &Q);
+	build(1, 1, MAX-1);
+	char cmd[10];
 
-			printf("ADD\n");
-
-			int K, P; scanf("%d %d", &K, &P);
-
-			// add K more cones at price P
-			counter.update(P, K);
-			counter.update(P+1, -K);
-
-			costb.update(P, K);
-			costb.update(P+1, -K);
-
-			numcones += 1LL*K;
-			totcost += 1LL*K*P;
-
-		} else if (strcmp(op, "ADDRANGE") == 0) {
-
-			printf("ADDRANGE\n");
-
-			int A, B; scanf("%d %d", &A, &B);
-
-			// add 1 cone to each price P in [A, B].
-
-			counter.update(A, 1);
-			counter.update(B+1, -1);
-
-			costm.update(A, 1);
-			costb.update(A, -A+1);
-
-			costm.update(B+1, -1);
-			costb.update(B+1, 1-A);
-
-			numcones += 1LL*(B-A+1);
-
-			// A + A+1 + ... + B = B*(B+1)/2 - A*(A-1)/2
-			totcost += 1LL*B*(B+1)/2 - 1LL*A*(A-1)/2;
-
-		} else if (strcmp(op, "BUYAMT") == 0) {
-
+	while (Q--) {
+		scanf(" %s", cmd);
+		if (strcmp(cmd, "ADD") == 0) {
+			ll K; int P; scanf("%lld %d", &K, &P);
+			update(1, P, P, K);
+		} else if (strcmp(cmd, "ADDRANGE") == 0) {
+			int A, B; scanf("%d%d", &A, &B);
+			update(1, A, B, 1LL);
+		} else if (strcmp(cmd, "BUYAMT") == 0) {
 			ll Q; scanf("%lld", &Q);
-
-			// Do a binary search. slightly sketchy.
-
-			ll x = 0;
-			ll curm=0, curb=0;
-			for (int k = LOG-1; k >= 0; --k) {
-				if (x+(1<<k) < MAX) {
-
-					ll newb = curb + costb.b[x+(1<<k)];
-					ll newm = curm + costm.b[x+(1<<k)];
-
-					ll newx = x + (1LL<<k);
-
-					if (newm * newx + newb < Q) {
-						curm = newm;
-						curb = newb;
-						x = newx;
-					}
-				}
+			ll ans = buyamt(1, Q);
+			printf("%lld\n", ans);
+		} else if (strcmp(cmd, "BUYLOW") == 0) {
+			ll L; scanf("%lld", &L);
+			printf("%lld\n", buylow(1, L));
+		} else if (strcmp(cmd, "BUYHIGH") == 0) {
+			ll L; scanf("%lld", &L);
+			printf("%lld\n", buyhigh(1, L));
+		} else if (strcmp(cmd, "COST") == 0) {
+			ll L; scanf("%lld", &L);
+			node root = query(1, 1, MAX-1);
+			if (root.cnt < L) printf("-1\n");
+			else {
+				printf("%lld\n", cost(1, L));
 			}
-
-			ll cost = curm * x + curb;
-			ll rem = Q-cost;
-			++x;
-			cost += (rem/x) * x;
-
-			printf("%lld\n", cost);
-
-		} else if (strcmp(op, "BUYLOW") == 0) {
-
-			ll L; scanf("%lld", &L); 
-
-			ll cost = buylow(L);
-
-			printf("%lld\n", cost);
-
-		} else if (strcmp(op, "BUYHIGH") == 0) {
-
-			ll L; scanf("%lld", &L); 
-
-			ll cost = totcost - buylow(numcones-L);
-
-			printf("%lld\n", cost);
-
-		} else if (strcmp(op, "COST") == 0) {
-
-			int L; scanf("%d", &L);
-			int cost = counter.lowerbound(L);
-			if (cost == MAX) printf("-1\n");
-			else printf("%d\n", cost);
-
-		} else if (strcmp(op, "NUMCONES") == 0) {
-
-			printf("%lld\n", numcones);
+		} else if (strcmp(cmd, "NUMCONES") == 0) {
+			node root = query(1, 1, MAX-1);
+			printf("%lld\n", root.cnt);
 		} else {
-			// total cost
-			printf("%lld\n", totcost);
+			node root = query(1, 1, MAX-1);
+			printf("%lld\n", root.cost);
 		}
 	}
 }
